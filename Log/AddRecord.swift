2fe02945e3d6232e08e8798fd2db59399c607f0c
MikeWong <mikewong.hzh@autonavi.com>
2023-07-07 20:32:27 +0800

    feature:添加了自定义标签的能力

--- Log/AddRecord.swift
+++ Log/AddRecord.swift
@@ -6,18 +6,31 @@
 //
 
 import SwiftUI
+struct TagStyle:ViewModifier {
+    let fill:Color
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .padding(4)
+            .background {
+            Rectangle()
+                    .fill(fill)
+                    .cornerRadius(4)
+        }
+    }
+}
 
 struct AddRecord: View {
     
     @Environment(\.presentationMode) var presentationMode
     @Environment(\.dismiss) var dismiss
     @State private var title:String = ""
-    @State private var description: String = "我是一个描述"
+    @State private var description: String = ""
     @State private var beginDate: Date = Date()
     @State private var endDate: Date = Date()
     
     var body: some View {
-        VStack {
+        VStack (){
             Text("添加记录")
                 .font(.title2)
                 .fontWeight(.semibold)
@@ -25,20 +38,27 @@ struct AddRecord: View {
             TextField(text: $title) {
                 Text("输入标题")
             }
+            .submitLabel(.done)
             .padding(8)
             .cornerRadius(10)
             .background {
-                RoundedRectangle(cornerRadius: 4).foregroundColor(Color.gray)
+                RoundedRectangle(cornerRadius: 4)
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.009, brightness: 0.896))
+                    
             }
-            
+            HStack{
+                Text("记录详情:").font(.title3)
+                Spacer()
+            }
             TextEditor(text: $description)
                 .scrollContentBackground(.hidden)
-                .background(Color.secondary)
+                .background(Color(hue: 1.0, saturation: 0.009, brightness: 0.896))
                 .font(.system(size: 18))
                 .lineSpacing(4)
                 .cornerRadius(10)
                 .frame(maxWidth:500,maxHeight: 250)
-                .cornerRadius(5)
+                .cornerRadius(10)
+                .submitLabel(.done)
             HStack(spacing:10){
                 
                 DatePicker("开始", selection: $beginDate, displayedComponents: [.hourAndMinute])
@@ -51,20 +71,27 @@ struct AddRecord: View {
             }.frame(maxWidth:.infinity)
 
             HStack {
-                Text("选择标签:")
+                Text("选择标签:").padding(.vertical, 8)
                 Spacer()
             }
             
             HStack{
                 Text("家务")
+                    .modifier(TagStyle(fill:.red))
                 Text("工作")
+                    .modifier(TagStyle(fill:.blue))
                 Button {
                     debugPrint("clicked plus button")
                 } label: {
                     Image(systemName: "plus")
+                        .font(.title3)
+                        .fontWeight(.bold)
                 }
                 Spacer()
             }
+            ColorPicker(selection: /*@START_MENU_TOKEN@*/.constant(.red)/*@END_MENU_TOKEN@*/, label: {
+                Text("选择标签的颜色:")
+            }).padding(.top, 10)
             Spacer()
             Button {
                 debugPrint("end Date is \(endDate)")
@@ -74,12 +101,13 @@ struct AddRecord: View {
                     presentationMode.wrappedValue.dismiss()
                 }
             } label: {
-                
                 Text("完成")
+                    .font(.title2)
+                    .fontWeight(.bold)
                     .foregroundColor(.white)
                     .background {
                         RoundedRectangle(cornerRadius: 10.0)
-                            .frame(width: 100, height: 40)
+                            .frame(width: 300, height: 60)
                     }
         }
         }.padding(10)
